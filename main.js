b4db685575a4bcf0830d6225a5932134009eee1d
cristianccgg <cristianccggg@gmail.com>
2024-06-20 21:06:04 -0500

    javascript completed

--- main.js
+++ main.js
@@ -10,11 +10,22 @@ form.addEventListener("submit", (e) => {
   const year = document.getElementById("year").value.trim();
   const cvc = document.getElementById("cvc").value.trim();
   const msg = document.getElementById("message");
+  const continueBtn = document.getElementById("continue-btn");
+  const cardNum = document.getElementById("card-numbers");
+  const cardName = document.getElementById("card-name");
+  const cardMonth = document.getElementById("card-month");
+  const cardYear = document.getElementById("card-year");
+
+  const originalCardNum = cardNum.innerText;
+  const originalCardName = cardName.innerText;
+  const originalMonth = cardMonth.innerText;
+  const originalYear = cardYear.innerText;
 
   if (cardHolder === "") {
     valid = false;
     showError("cardholder");
   } else {
+    cardName.innerText = cardHolder.toUpperCase();
     hideError("cardholder");
   }
 
@@ -22,17 +33,20 @@ form.addEventListener("submit", (e) => {
     valid = false;
     showError("card-number");
   } else {
+    cardNum.innerText = cardNumber;
     hideError("card-number");
   }
 
-  if (month === "" || year === "") {
+  if (month === "" || month > 12 || year === "" || year <= 24) {
     valid = false;
     showError("date");
   } else {
+    cardMonth.innerText = month;
+    cardYear.innerText = year;
     hideError("date");
   }
 
-  if (cvc === "") {
+  if (cvc === "" || !/^\d{3,4}$/.test(cvc)) {
     valid = false;
     showError("cvc");
   } else {
@@ -41,10 +55,35 @@ form.addEventListener("submit", (e) => {
 
   if (valid) {
     msg.classList.remove("active");
-    setTimeout(() => {
-      msg.classList.add("active");
-      form.reset();
-    }, 3000);
+    form.classList.add("active");
+  }
+
+  continueBtn.addEventListener("click", () => {
+    msg.classList.add("active");
+    form.classList.remove("active");
+    form.reset();
+    cardNum.innerText = originalCardNum;
+    cardName.innerText = originalCardName;
+    cardMonth.innerText = originalMonth;
+    cardYear.innerText = originalYear;
+  });
+});
+
+month.addEventListener("input", function () {
+  if (month.value.length > 2) {
+    month.value = month.value.slice(0, 2);
+  }
+});
+
+year.addEventListener("input", function () {
+  if (year.value.length > 2) {
+    year.value = year.value.slice(0, 2);
+  }
+});
+
+cvc.addEventListener("input", function () {
+  if (this.value.length > 3) {
+    this.value = year.value.slice(0, 3);
   }
 });
 
